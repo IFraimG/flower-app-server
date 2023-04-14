2b868ec461517a6e7708e856cdb6fb51c42cf86a
IFramex <kulsasha55@gmail.com>
2023-04-14 16:59:57 +0300

    edit fix

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+// require("./configs/db")
+require("dotenv").config()
+
+// const indexRouter = require('./routes/index');
+// const gettersRouter = require('./routes/getters');
+// const settersRouter = require('./routes/setters');
+// const advertisementsRouter = require('./routes/advertisements');
+
+const app = express();
+app.use(cors())
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// app.use('/', indexRouter);
+// app.use('/getters', gettersRouter);
+// app.use('/setters', settersRouter);
+// app.use('/advertisements', advertisementsRouter);
+
+
+app.listen(process.env.PORT || 8000, () => console.log("сервер запущен 8000"))

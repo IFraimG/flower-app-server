6fb6d4a9a78a6d00152ff6f8bea6edc691464d13
IFramex <kulsasha55@gmail.com>
2023-05-06 19:40:42 +0300

    get_pin_market

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Getter = require("../models/Getter")
+const Setter = require("../models/Setter")
+
 
 router.get('/', function(req, res, next) {
   res.send("iii")
 });
+
+router.get("/get_pin_market/:typeUser/:userID", async (req, res) => {
+  if (req.params.typeUser == "setter") {
+    try {
+      let setter = Setter.findOne({ X5_ID: req.params.typeUser })
+      if (setter?.market == null) return res.sendStatus(404)
+      else return res.send(setter.market).status(200)
+    } catch (error) {
+      console.log(error.message);
+      return res.sendStatus(400)
+    }
+  } else {
+    try {
+      let getter = Getter.findOne({ X5_ID: req.params.typeUser })
+      if (getter?.market == null) return res.sendStatus(404)
+      else return res.send(getter.market).status(200)
+    } catch (error) {
+      console.log(error.message);
+      return res.sendStatus(400)
+    }
+  }
+})
 
 module.exports = router;

3fefaeb3c5e6ebb5aaaa808e3ad9b05adc2d2f1c
IFramex <kulsasha55@gmail.com>
2023-05-09 15:25:28 +0300

    market

--- routes/index.js
+++ routes/index.js
@@ -12,20 +12,20 @@ router.get("/get_pin_market", async (req, res) => {
   if (req.query.typeUser == "setter") {
     try {
       let setter = await Setter.findById(req.query.userID)
-      if (setter?.market == null) return res.sendStatus(404)
-      else return res.send(setter.market).status(200)
+      if (setter?.market == null) return res.status(404).send({ message: "Error" })
+      else return res.status(200).send({market: setter.market})
     } catch (error) {
       console.log(error.message);
-      return res.sendStatus(400)
+      return res.status(404).send({ message: "Error" })
     }
   } else {
     try {
       let getter = await Getter.findById(req.query.userID)
-      if (getter?.market == null) return res.sendStatus(404)
-      else return res.send(getter.market).status(200)
+      if (getter?.market == null) return res.status(404).send({message: "Error"})
+      else return res.status(200).send({market: getter.market})
     } catch (error) {
       console.log(error.message);
-      return res.sendStatus(400)
+      return res.status(404).send({ message: "Error" })
     }
   }
 })

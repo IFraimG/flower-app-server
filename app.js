a243928746dd323fd4e0e42e776b625e8325a011
IFraimG <kulsasha55@gmail.com>
2023-09-16 14:54:07 +0300

    iii

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 

64ad87684a2f8ebfaaad76bebc58f9fa82258fe0
IFraimG <kulsasha55@gmail.com>
2023-10-25 11:44:06 +0300

    redwd

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const usersController = require("../controllers/user.controller")
 let passport = require("../configs/passportConfig.js")
 let upload = require("../configs/upload.js")
 
-router.put("/edit_profile", upload.array("img"), passport.authenticate('jwt', { session: false }), usersController.editProfile)
+router.put("/edit_profile", passport.authenticate('jwt', { session: false }), usersController.editProfile)
 router.post("/add_photo", upload.single("img"), passport.authenticate('jwt', { session: false }), usersController.addPhoto)
 router.put("/change_scores", passport.authenticate('jwt', { session: false }), usersController.changeScores)
 router.get("/get_user", passport.authenticate('jwt', { session: false }), usersController.getUserByID)
